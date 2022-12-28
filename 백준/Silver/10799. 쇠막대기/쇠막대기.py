9f77dbf2c6686e32e6555bf235882ae38e377b7f
s6m1n <46596035+s6m1n@users.noreply.github.com>
2022-12-28 13:59:08 +0900

    [Silver II] Title: 쇠막대기, Time: 56 ms, Memory: 31684 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.py
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.py
@@ -6,10 +6,9 @@ for i in range(len(a)):
   if a[i]=='(':
     s.append('(')
   else:
+    s.pop() # 레이저 제거
     if a[i-1]=='(': # 레이저면
-      s.pop() # 레이저 제거
       cnt+=len(s) # 스택에 있는 괄호 수(막대기 수)만큼 추가
     else: # 막대기의 끝이면
-      s.pop() # 막대기 제거
       cnt+=1 # 한 조각 추가
 print(cnt)
