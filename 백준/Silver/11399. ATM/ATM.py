8855ee3b8c40dfdc33d72a00a89cc63736afe0b2
s6m1n <46596035+s6m1n@users.noreply.github.com>
2023-01-06 14:27:03 +0900

    [Silver IV] Title: ATM, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,8 +1,4 @@
 n=int(input()) #명 수
-time = list(map(int,input().split())) #시간 리스트
-time.sort()
 cnt=0
-for t in time:
-  cnt+=t*n
-  n-=1
+for t in sorted(list(map(int,input().split()))):cnt+=t*n;n-=1
 print(cnt)
