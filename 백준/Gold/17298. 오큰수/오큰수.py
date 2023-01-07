6a5fc880ccdb0ade06aaadc0ad2518769d57f08c
s6m1n <46596035+s6m1n@users.noreply.github.com>
2023-01-07 18:02:13 +0900

    [Gold IV] Title: 오큰수, Time: 1328 ms, Memory: 154524 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -15,3 +15,22 @@ for idx in range(1,N):
       answer[idx_s.pop()] = max
     idx_s.append(idx)
 print(*answer)
+
+# for문 돌며 idx 한 칸씩 증가
+# stack 검사하기
+## 비어있으면 그냥 넣기
+## s[-1]이랑 같거나 작으면 넣기, s[-1]보다 큰 애 나올 때까지 s.pop()하면서 값 바꿔주고 마지막에 자기 자신 append
+
+# nums = list(map(int,input().split()))
+# nums.reverse()
+# answer=[]
+# while nums:
+#   N = nums.pop(); max = -1
+#   for n in nums[::-1]:
+#     if N<n: max = n; break
+#   answer.append(max)
+# print(*answer)
+
+# 9 5 5 2 3 6 1 3 2 5  4  9  8  5  2
+# 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+# - 7 
