da005b11c38821df8311e6d32dc6c2417ef672a0
s6m1n <46596035+s6m1n@users.noreply.github.com>
2023-01-07 09:26:49 +0900

    [Silver I] Title: 회의실 배정, Time: 300 ms, Memory: 52852 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,14 +1,14 @@
 from collections import deque
-N = int(input())
+from sys import stdin
+N = int(stdin.readline().rstrip())
 d=[]
-[d.append(list(map(int,input().split()))) for _ in range(N)]
+[d.append(list(map(int,stdin.readline().split()))) for _ in range(N)]
 #리스트에 수 저장
 d=deque(sorted(d))
 min = d.popleft()
 answer=0
 while d:
   tmp = d.popleft()
-#  for i in range(start, N):
   if tmp[1]<min[1]:
     min=tmp
   elif min[1]<=tmp[0]: # 끝에 오면
