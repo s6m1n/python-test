4d391a59d91e1ebcb453cf551b18711dc2db1ae9
s6m1n <46596035+s6m1n@users.noreply.github.com>
2023-01-07 10:58:57 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -0,0 +1,9 @@
+from sys import stdin
+b=[]
+a = [i for i in range(1,31)]
+for _ in range(28):
+  tmp=int(stdin.readline().rstrip())
+  b.append(tmp)
+b=sorted(list(set(a)-set(b)))
+print(b[0])
+print(b[1])
