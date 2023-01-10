76a1562e016955d8660c40081edc68444c9f07a3
s6m1n <46596035+s6m1n@users.noreply.github.com>
2023-01-10 14:43:41 +0900

    [Bronze II] Title: 문자열 반복, Time: 32 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -1,13 +1,6 @@
 N=int(input())
-M=[]
-str=[]
 for n in range(N):
-  i,j=input().split()
-  M.append(int(i))
-  str.append(j)
-for n in range(N): 
-  s=[t for t in str[n]]
-  for idx in range(len(s)):
-    for m in range(M[n]):
-      print(s[idx], end='')
-  print('')
+  M, str=input().split()
+  for s in str:
+      print(s*int(M), end='')
+  print()
